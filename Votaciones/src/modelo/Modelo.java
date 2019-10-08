78bbb5a57fc98bc884d250179c7d8e92c4834921
alberto.rivero <alberto.rivero@DGFA124106CGP.inet.uady.mx>
2019-10-08 18:32:52 -0500

    Modelo

--- Votaciones/src/modelo/Modelo.java
+++ Votaciones/src/modelo/Modelo.java
@@ -1,10 +1,44 @@
 
 package modelo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author julio-cabrera
  */
 public class Modelo {
+      
+    public int sumarArchivo(String direccion){
+        //Principal pr = new Principal();
+        int sumador = 0;
+        File ruta = new File(direccion);
+        try{
+            FileReader fr = new FileReader(direccion);
+            BufferedReader br = new BufferedReader(fr);
+            String cadena = null;
+            
+            while((cadena = br.readLine()) != null){
+                StringTokenizer dato = new StringTokenizer(cadena," ");
+              
+                 sumador++;
+             
+              
+            
+            }
+           return sumador;
+        
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex.getStackTrace());
+        }
+    
+       return sumador;
+    }
+    
+   
     
 }
