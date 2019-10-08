7b520ec1c406b51347d2430988ae0e0426c5b081
juliocabrera <juliocabrera820@gmail.com>
2019-10-08 14:12:50 -0500

    Grafica de barras con valores predeterminados

--- Votaciones/src/principal/Principal.java
+++ Votaciones/src/principal/Principal.java
@@ -1,14 +1,41 @@
-
 package principal;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
  * @author julio-cabrera
  */
 public class Principal {
-    
+
     public static void main(String[] args) {
-        
+        /**
+         * Ejemplo con valores por defecto
+         */
+        //Grafica de barras
+        generarBarras();
+
     }
-    
+
+    public static void generarBarras() {
+        try {
+            DefaultCategoryDataset data = new DefaultCategoryDataset();
+            data.addValue(20, "Persona", "Candidato 1");
+            data.addValue(25, "Persona", "Candidato 2");
+            data.addValue(40, "Persona", "Candidato 3");
+
+            JFreeChart jfre = ChartFactory.createBarChart3D("Votaciones", "Candidatos", "Votos", data, PlotOrientation.VERTICAL, true, true, true);
+            ChartFrame frame = new ChartFrame("Votos",jfre);
+            frame.setSize(600, 400);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        } catch (Exception e) {
+            System.out.println("Error "+e);
+        }
+    }
+
 }
