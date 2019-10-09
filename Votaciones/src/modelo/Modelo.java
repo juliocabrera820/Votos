865f48e8dd939807d153b7002b4ae5bd0e314e3a
juliocabrera <juliocabrera820@gmail.com>
2019-10-09 00:23:03 -0500

    Obtener fecha en tiempo real

--- Votaciones/src/modelo/Modelo.java
+++ Votaciones/src/modelo/Modelo.java
@@ -1,12 +1,14 @@
 package modelo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.StringTokenizer;
 import javax.swing.JOptionPane;
 
 /**
@@ -16,47 +18,50 @@ import javax.swing.JOptionPane;
 public class Modelo {
 
     private Grafica grafica;
-           
+
     public Modelo() {
         grafica = new Grafica();
     }
 
-    public int sumarArchivo(String direccion) {
-        //Principal pr = new Principal();
-        int sumador = 0;
-        File ruta = new File(direccion);
+    public ArrayList<String> leerArchivo(String ruta) {
+        ArrayList<String> productos = new ArrayList<>();
         try {
-            FileReader fr = new FileReader(direccion);
-            BufferedReader br = new BufferedReader(fr);
-            String cadena = null;
-
-            while ((cadena = br.readLine()) != null) {
-                StringTokenizer dato = new StringTokenizer(cadena, " ");
-                sumador++;
-            }
-            return sumador;
-
+            productos = (ArrayList<String>) Files.lines(Paths.get(ruta));
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, ex.getStackTrace());
         }
-
-        return sumador;
+        return productos;
     }
 
-    public String obtenerFecha(){
-        
-        Date Fecha = new Date();
-        DateFormat fecha = new SimpleDateFormat("HH:mm:ss dd/MM/YYYY");
-        return fecha.toString();
-    }
- 
-    public void graficaBarra() {
-        grafica.generarBarras();
+    public void escribirArchivo(String ruta) {
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try {
+            fichero = new FileWriter(ruta, true);
+            pw = new PrintWriter(fichero);
+            pw.println(obtenerFecha());
+            pw.close();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, e.getStackTrace());
+        }
     }
 
-    public void graficaPastel() {
-        grafica.generarPastel();
+    public int numeroVotos(ArrayList<String> votos) {
+        return votos.size();
     }
-    
-    //TODO OBTENER FECHA DEL VOTO
+
+    public String obtenerFecha() {
+        Date fecha = new Date();
+        DateFormat formato = new SimpleDateFormat("HH:mm:ss dd/MM/YYYY");
+        return formato.format(fecha);
+    }
+
+    public void graficaBarra(ArrayList<String> votos) {
+        grafica.generarBarras(votos);
+    }
+
+    public void graficaPastel(ArrayList<String> votos) {
+        grafica.generarPastel(votos);
+    }
+
 }
