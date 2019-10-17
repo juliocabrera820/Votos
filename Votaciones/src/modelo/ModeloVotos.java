e0bfda118753358047e287fe6a5d59ffbd991a44
juliocabrera <juliocabrera820@gmail.com>
2019-10-17 12:31:41 -0500

    Map de productos y generar graficas

--- Votaciones/src/modelo/ModeloVotos.java
+++ Votaciones/src/modelo/ModeloVotos.java
@@ -10,8 +10,17 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import javax.swing.JOptionPane;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 
 /**
  *
@@ -65,6 +74,56 @@ public class ModeloVotos {
             } catch (IOException ex) {
                 JOptionPane.showInputDialog(null, ex.getStackTrace());
             }
+        }
+    }
+
+    public Map<String, Integer> votosProductos(ArrayList<String> productos) {
+        Map<String, Integer> votos = new HashMap<String, Integer>();
+        for (String producto : productos) {
+            try {
+                File archivo = new File("/home/julio-cabrera/ArchivosProductos/" + producto + ".txt");
+                if (!archivo.exists()) {
+                    votos.put(producto, 0);
+                } else {
+                    int votosProducto = (int) Files.lines(Paths.get("/home/julio-cabrera/ArchivosProductos/" + producto + ".txt")).count();
+                    votos.put(producto, votosProducto);
+                }
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, ex.getStackTrace());
+            }
+        }
+        return votos;
+    }
+
+    public void generarBarras(Map<String, Integer> productos) {
+        try {
+            DefaultCategoryDataset data = new DefaultCategoryDataset();
+            for (Entry<String, Integer> producto : productos.entrySet()) {
+                data.addValue(producto.getValue(), "Votos", producto.getKey());
+            }
+            JFreeChart jfre = ChartFactory.createBarChart3D("Votaciones", "Productos", "Votos", data, PlotOrientation.VERTICAL, true, true, true);
+            ChartFrame frame = new ChartFrame("Votos", jfre);
+            frame.setSize(600, 400);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        } catch (Exception e) {
+            System.out.println("Error " + e);
+        }
+    }
+
+    public void generarPastel(Map<String, Integer> productos) {
+        try {
+            DefaultPieDataset data = new DefaultPieDataset();
+            for (Entry<String, Integer> producto : productos.entrySet()) {
+                data.setValue(producto.getKey(), producto.getValue());
+            }
+            JFreeChart jfree = ChartFactory.createPieChart3D("Votaciones", data, true, true, true);
+            ChartFrame frame = new ChartFrame("Votaciones", jfree);
+            frame.setSize(600, 400);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        } catch (Exception e) {
+            System.out.println("Error " + e);
         }
     }
 
