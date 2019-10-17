80d53ed4febf46c475c757d779c25c2ce956bcea
juliocabrera <juliocabrera820@gmail.com>
2019-10-17 11:24:04 -0500

    Principal

---
+++ Votaciones/src/principal/PrincipalVotos.java
@@ -0,0 +1,18 @@
+
+package principal;
+
+import controlador.ControladorVotos;
+
+
+/**
+ *
+ * @author julio-cabrera
+ */
+public class PrincipalVotos {
+    
+    public static void main(String[] args) {
+        ControladorVotos controlador = new ControladorVotos();
+        controlador.iniciar();
+    }
+    
+}
