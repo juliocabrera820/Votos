835e07a4babb2d4ec8aa39ab68e00043ff5dbb72
juliocabrera <juliocabrera820@gmail.com>
2019-10-17 12:31:10 -0500

    Actualizar votaciones y mostrar votos al momento cuando se ejecuta

--- Votaciones/src/controlador/ControladorVotos.java
+++ Votaciones/src/controlador/ControladorVotos.java
@@ -3,6 +3,7 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Map;
 import javax.swing.JOptionPane;
 import modelo.ModeloVotos;
 import vista.VistaVotos;
@@ -16,12 +17,14 @@ public class ControladorVotos implements ActionListener {
     private ModeloVotos modelo;
     private VistaVotos vista;
     private ArrayList<String> productos;
+    private Map<String, Integer> votos;
 
     public ControladorVotos() {
         modelo = new ModeloVotos();
         vista = new VistaVotos();
         productos = this.modelo.leerArchivoProductos("/home/julio-cabrera/ArchivosProductos/Productos.txt");
         agregarProductos();
+        votos = this.modelo.votosProductos(productos);
         this.vista.btnBarra.addActionListener(this);
         this.vista.btnPastel.addActionListener(this);
         this.vista.btnVotar.addActionListener(this);
@@ -37,6 +40,12 @@ public class ControladorVotos implements ActionListener {
             } else {
                 this.modelo.votar(producto);
             }
+        } else if (this.vista.btnBarra == ae.getSource()) {
+            this.votos = this.modelo.votosProductos(productos);
+            this.modelo.generarBarras(votos);
+        } else if (this.vista.btnPastel == ae.getSource()) {
+            this.votos = this.modelo.votosProductos(productos);
+            this.modelo.generarPastel(votos);
         }
     }
 
@@ -51,6 +60,10 @@ public class ControladorVotos implements ActionListener {
         this.vista.setSize(700, 440);
         this.vista.setLocationRelativeTo(null);
         this.vista.setVisible(true);
+    }
+
+    public void votacionesEquipos() {
+        votos.forEach((k, v) -> System.out.println("Producto " + k + " votos: " + v));
     }
 
 }
